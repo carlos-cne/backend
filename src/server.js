4c4cba6730de7d553c6d5fbdd2f37ad1e8e42647
Carlos Queiroz <carlostgti@outlook.com>
2019-08-06 14:28:22 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const server = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@cluster0-9zthu.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
